65e3887d78d6e03b1324a7e3de8a7f261ae03b75
deepak4124 <deepaktarun1976@gmail.com>
2024-11-18 13:56:23 +0530

    chore: added responsivness

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-function Card({album}){
+function Card({ album }) {
     return (
-        <div className="hover:scale-[110%] scale-100 relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96 shadow shadow-2xl hover:opacity-70 duration-300">
-          <div className="relative h-70 m-5 overflow-hidden text-white rounded-md">
-            <img src={album.images[0]?.url} alt={album.name} className="w-full h-full object-cover " />
-          </div>
-          <div className="p-4">
-            <h6 className="mb-2 text-slate-800 text-xl font-semibold">
-              {album.name}
-            </h6>
-          </div>
+        <div className="hover:scale-105 scale-100 relative flex flex-col my-4 bg-white shadow-md border border-slate-200 rounded-lg w-full sm:w-60 md:w-64 lg:w-72 xl:w-80 hover:opacity-90 duration-300">
+            <div className="relative h-48 sm:h-52 md:h-60 lg:h-64 xl:h-72 m-4 overflow-hidden text-white rounded-md">
+                <img src={album.images[0]?.url} alt={album.name} className="w-full h-full object-cover" />
+            </div>
+            <div className="px-4 pb-4">
+                <h6 className="mb-2 text-slate-800 text-base sm:text-lg md:text-xl font-semibold">
+                    {album.name}
+                </h6>
+            </div>
         </div>
-      );
-    }
+    );
+}
 
-export default Card
+export default Card;
--- src/Test.jsx
+++ src/Test.jsx
@@ -3,8 +3,8 @@ import Search from './Search';
 import Albumpage from './Albumpage';
 import { Routes, Route } from 'react-router-dom';
 
-const CLIENT_ID = import.meta.env.CLIENT_ID;
-const CLIENT_SECRET=import.meta.env.CLIENT_SECRET;
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
 
 function Test() {
 
